8a6b5e58bce4791854197ce42f579798782743e7
secretmud <lamefeed@gmail.com>
2018-09-29 16:59:35 +0200

    Made it from scratch. propperly this time.

--- main.py
+++ main.py
@@ -1,75 +1,68 @@
 from time import time
-import csv
+import wikipedia
 import os
+from random import randint
+import threading
 
 
 
-words_easy = ["ape", "ananas", "penger", "test", "kanskje", 
-              "hvorfor", "hvordan", "hvor", "fordi", "tvilsomt", 
-              "adekvat", "skildring", "minister", "Kvakksalver",
-              "pendulum"]
-words_medium = ["kvakksalver", "Minst", "Konsentrasjon",
-                "Oppbygd", "Hostesaft", "Appelsinmarmelade"]
-words_hard = ["TuLlBaLL", "AppLikaSJon", "OrdenTLIg", "AvKobbLInG"]
-words_extreme = [";:/=+2$", "£€?/.#~]}", "!`\|@#~~#?-)", "^5*&$Hei"]
-while __name__ == "__main__":
-    max_score = 0
+search = ["Donald Knuth", "Brian Kernighan", "Dennis Richie", "Richard Stallman", "Linus Torvalds"]
+
+def game(word, difficulty):
+    start_time = time()
     correct = []
     wrong = []
-    start_time = time()
+    word_list = []
+    words = wikipedia.summary(word, sentences=difficulty)
+    word_list.extend(words.split())
+    points = (len(correct) - len(wrong))
+    if (len(correct)+len(wrong))< len(word_list):
+        for i in word_list:
+            print(pressed)
+            word_len_ans = ((len(correct)) + (len(wrong)))
+            os.system('clear' or 'cls')
+            strings = ' '.join(word_list)
+            print(strings)
+            ttime = (int(time() - start_time))
+            print(ttime)
+            print(word_len_ans, "/", len(word_list))
+            test = input()
+            if test in word_list:
+                correct.append(test)
+            else:
+                wrong.append(test)
+    if (len(correct)+len(wrong))>= len(word_list):
+        os.system('clear' or 'cls')
+        percentage_correct = ((len(correct)/len(word_list)*100))
+        wpm = ((len(words)/int(ttime))*int(percentage_correct))
+        max_points = len(word_list)
+        print("Score board:\nPoints: {0}/{1}\nWPM: {2}\nTime: {3}\n".format(points, max_points, wpm, ttime))
+        exit = input()
+        if exit == "Yes":
+            threading.stop()
+
+while __name__ == "__main__":
+    rand_search = randint(0, len(search) -1)
     os.system('clear' or 'cls')
-    print("This game will test your typing skills")
-    print("There are 4 levels to try:\n" +
+    print("This is the menu\n" +
           "\t1. Easy\n" +
           "\t2. Medium\n" +
           "\t3. Hard\n" +
           "\t4. Extreme\n\n")
-    difficulty = input("Choose your difficulty:")
-    words = []
-    if difficulty == "1":
-        words.extend(words_easy)
-        max_score = len(words_easy)
-        os.system('clear' or 'cls')
-        running = True
-    elif difficulty == "2":
-        words.extend(words_medium)
-        max_score = len(words_medium)
-        os.system('clear' or 'cls')
-        running = True
-    elif difficulty == "3":
-        words.extend(words_hard)
-        max_score = len(words_hard)
-        os.system('clear' or 'cls')
-        running = True
-    elif difficulty == "4":
-        words.extend(words_extreme)
-        max_score = len(words_extreme)
-        os.system('clear' or 'cls')
-        running = True
-    elif difficulty == "exit":
-        print("Thanks for playing!")
-        __name__ == "__spain__"
+    diff = input()
+    if diff == "1":
+        threading.Timer(0.5, game(search[rand_search], 2)).start()
+        
+    if diff == "2":
+        game(search[rand_search], 5)
+    if diff == "3":
+        game(search[rand_search], 7)
+    if diff == "4":
+        game(search[rand_search], 9)
     
-    while running:
-        for i in words:
-            print(i)
-            answer = input("")
-            print(words)
-            if answer == i:
-                correct.append(i)
-                words.remove(i)
-            elif answer != i:
-                wrong.append(i)
-                words.remove(i) 
-            os.system('clear' or 'cls')
-            if len(words) == 0:
-                tot_time = time() - start_time
-                print("You made it with:")
-                print(str(len(correct)) + "/" + str(max_score))
-                x = len(correct)/(tot_time/60)
-                print("that score would be equal to", x, "WPM")
-                print("Time:", tot_time)
-                print(correct)
-                print(wrong)
-                x = input("Press Enter to contiue")
-                running = False
+
+
+
+
+
+
